f582f84ecad8f9fea8803592e613155ca9aa081a
eduardorgv <eduardsabb@outlook.com>
2024-07-31 15:11:04 -0600

    main structure folder

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <h1>Order #ddf8 Page</h1>
+    </div>
+  );
+}
---
+++ src/config/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Quicksand } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const quicksand = Quicksand({ subsets: ["latin"] });
